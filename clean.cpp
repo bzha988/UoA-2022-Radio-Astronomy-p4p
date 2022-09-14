3d6b5cb4349c7a70ad7786b774630cd165c2f84e
yeefan822 <68040785+yeefan822@users.noreply.github.com>
2022-09-15 11:32:46 +1200

    Create three shared memories to enable non-mutable variables

--- clean.cpp
+++ clean.cpp
@@ -28,7 +28,7 @@ static auto exception_handler = [](sycl::exception_list e_list) {
 };
 int perform_clean(queue& q, float *dirty, float *psf, float gain, int iters, float *local_max_x,
 	float *local_max_y, float *local_max_z, float *model_l, float *model_m, float *model_intensity,int *d_source_c,
-	float *max_xyz,float *running_avg, float *operation_count) {
+	float *max_xyz,float *running_avg, float *operation_count, float* maxx) {
 	int image_size = 8;
 	int cycle_number = 0;
 	float flux = 0.0;
@@ -42,26 +42,28 @@ int perform_clean(queue& q, float *dirty, float *psf, float gain, int iters, flo
 	//Find max row reduct
 	for (int i = 0; i < 6; i++) {
 		auto h = q.parallel_for(num_rows, [=](auto j) {
-			float max_x = float(0);
-			float max_y = fabs(dirty[j * 8]);
-			float max_z = dirty[j * 8];
+			 maxx[0] = float(0);
+			 maxx[1] = fabs(dirty[j * 8]);
+			 maxx[2] = dirty[j * 8];
 
-		float current;
-		for (int col_index = 1; col_index < 8; ++col_index)
-		{
-			current = dirty[j * 8 + col_index];
+		
+		auto u = q.parallel_for(num_rows, [=](auto b) {
+			float current;
+			current = dirty[j * 8 + b];
 
-			max_y += fabs(current);
-			if (fabs(current) > fabs(max_z))
+			maxx[1] += fabs(current);
+			if (fabs(current) > fabs(maxx[2]))
 			{
-				max_x = (float)col_index;
-				max_z = current;
+				maxx[0] = (float)b;
+				maxx[2] = current;
 			}
-		}
+			});
+		u.wait();
+		
 
-		local_max_x[j] = max_x;
-		local_max_y[j] = max_y;
-		local_max_z[j] = max_z;
+		local_max_x[j] = maxx[0];
+		local_max_y[j] = maxx[1];
+		local_max_z[j] = maxx[2];
 		});
 		h.wait();
 
@@ -242,6 +244,7 @@ int main() {
 		float* local_max_x = malloc_shared<float>(image_size, q);
 		float* local_max_y = malloc_shared<float>(image_size, q);
 		float* local_max_z = malloc_shared<float>(image_size, q);
+		float* max = malloc_shared<float>(three_d, q);
 		float* max_xyz = malloc_shared<float>(three_d, q);
 		int* d_source_c = malloc_shared<int>(single_element, q);
 		d_source_c[0] = 0;
@@ -265,7 +268,7 @@ int main() {
 		auto start = high_resolution_clock::now();
 		
 		int number_of_cycle=perform_clean(q, dirty, psf, gain, iters, local_max_x,
-			local_max_y, local_max_z, model_l, model_m, model_intensity, d_source_c,max_xyz,running_avg,operation_count);
+			local_max_y, local_max_z, model_l, model_m, model_intensity, d_source_c,max_xyz,running_avg,operation_count,max);
 		
 		// stop timer
 		auto stop = high_resolution_clock::now();
