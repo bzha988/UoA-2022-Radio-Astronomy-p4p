19156a03cd78a062d59fa93e6da71c2a1fc89dc8
yeefan822 <68040785+yeefan822@users.noreply.github.com>
2022-09-15 13:10:17 +1200

    Update clean.cpp

--- clean.cpp
+++ clean.cpp
@@ -26,9 +26,9 @@ static auto exception_handler = [](sycl::exception_list e_list) {
 		}
 	}
 };
-int perform_clean(queue& q, float *dirty, float *psf, float *local_max_x,
-	float *local_max_y, float *local_max_z, float *model_l, float *model_m, float *model_intensity,int *d_source_c,
-	float *max_xyz,float *running_avg, float *operation_count) {
+int perform_clean(queue& q, float* dirty, float* psf, float gain, int iters, float* local_max_x,
+	float* local_max_y, float* local_max_z, float* model_l, float* model_m, float* model_intensity, int* d_source_c,
+	float* max_xyz, float* running_avg, float* operation_count) {
 	int image_size = 8;
 	int cycle_number = 0;
 	float flux = 0.0;
@@ -38,7 +38,7 @@ int perform_clean(queue& q, float *dirty, float *psf, float *local_max_x,
 	float weak_source_percent = 0.01;
 	float noise_detection_factor = 2.0;
 	range<1> num_rows{ 8 };
-	
+
 	//Find max row reduct
 	for (int i = 0; i < 6; i++) {
 		auto h = q.parallel_for(num_rows, [=](auto j) {
@@ -46,23 +46,23 @@ int perform_clean(queue& q, float *dirty, float *psf, float *local_max_x,
 			float max_y = fabs(dirty[j * 8]);
 			float max_z = dirty[j * 8];
 
-		float current;
-		for (int col_index = 1; col_index < 8; ++col_index)
-		{
-			current = dirty[j * 8 + col_index];
-
-			max_y += fabs(current);
-			if (fabs(current) > fabs(max_z))
+			float current;
+			for (int col_index = 1; col_index < 8; ++col_index)
 			{
-				max_x = (float)col_index;
-				max_z = current;
-			}
-		}
+				current = dirty[j * 8 + col_index];
 
-		local_max_x[j] = max_x;
-		local_max_y[j] = max_y;
-		local_max_z[j] = max_z;
-		});
+				max_y += fabs(current);
+				if (fabs(current) > fabs(max_z))
+				{
+					max_x = (float)col_index;
+					max_z = current;
+				}
+			}
+
+			local_max_x[j] = max_x;
+			local_max_y[j] = max_y;
+			local_max_z[j] = max_z;
+			});
 		h.wait();
 
 		//Find max col reduct
@@ -89,24 +89,24 @@ int perform_clean(queue& q, float *dirty, float *psf, float *local_max_x,
 		// substract psf values for input
 		running_avg[0] /= (image_size * image_size);
 		const int half_psf = 8 / 2;
-		float *avg = &running_avg[0];
-		float *zc = &max_xyz[2];
+		float* avg = &running_avg[0];
+		float* zc = &max_xyz[2];
 		float* zero_index = &model_intensity[0];
-		bool extracting_noise = *zc < noise_detection_factor * *avg * loop_gain;
-		bool weak_source = *zc < *zero_index * weak_source_percent;
-		
+		bool extracting_noise = *zc < noise_detection_factor** avg* loop_gain;
+		bool weak_source = *zc < *zero_index* weak_source_percent;
+
 		if (extracting_noise || weak_source) {
 			return num_cy;
 		}
-		
+
 		model_l[d_source_c[0]] = max_xyz[0];
 		model_m[d_source_c[0]] = max_xyz[1];
 		model_intensity[d_source_c[0]] = max_xyz[2];
-		
-		
+
+
 		d_source_c[0] += 1;
 		for (int i = 0; i < 8; i++) {
-			auto e = q.parallel_for(num_rows, [=](auto k){
+			auto e = q.parallel_for(num_rows, [=](auto k) {
 				int image_coord_x = model_l[d_source_c[0]] - half_psf + i;
 				int image_coord_y = model_m[d_source_c[0] - 1] - half_psf + k;
 				float psf_weight = psf[k * 8 + i];
@@ -115,6 +115,7 @@ int perform_clean(queue& q, float *dirty, float *psf, float *local_max_x,
 				operation_count[1] = dirty[image_coord_y * 8 + image_coord_x];
 				operation_count[2] = psf_weight * model_intensity[d_source_c[0] - 1];
 				});
+			e.wait();
 			std::cout << "Operation: ";
 			std::cout << operation_count[0] << "\n";
 			std::cout << operation_count[1] << "\n";
@@ -122,8 +123,8 @@ int perform_clean(queue& q, float *dirty, float *psf, float *local_max_x,
 
 		}
 
-		
-		auto f = q.parallel_for(num_rows, [=](auto m)  {
+
+		auto f = q.parallel_for(num_rows, [=](auto m) {
 			float last_source_x = model_l[d_source_c[0] - 1];
 			float last_source_y = model_m[d_source_c[0] - 1];
 			float last_source_z = model_intensity[d_source_c[0] - 1];
@@ -137,6 +138,7 @@ int perform_clean(queue& q, float *dirty, float *psf, float *local_max_x,
 				}
 			}
 			});
+		f.wait();
 		num_cy++;
 
 	}
@@ -221,13 +223,16 @@ int main() {
 	default_selector d_selector;
 #endif
 	size_t image_size = 8;
+	size_t psf_size = 8;
 	size_t size_square = 8 * 8;
 	size_t number_cycles = 60;
 	size_t single_element = 1;
 	size_t three_d = 3;
-	
+
 	try {
 		queue q(d_selector, exception_handler);
+		float gain = 0.1;
+		int iters = 60;
 		float* dirty = malloc_shared<float>(size_square, q);
 		float* psf = malloc_shared<float>(size_square, q);
 		float* model_l = malloc_shared<float>(number_cycles, q);
@@ -249,34 +254,36 @@ int main() {
 		strcpy(dirty_image, "dirty.csv");
 		char* psf_image = new char[7];
 		strcpy(psf_image, "psf.csv");
-		char* output_img=new char[7];
+		char* output_img = new char[7];
 		strcpy(output_img, "img.csv");
 		char* output_src = new char[10];
 		strcpy(output_src, "source.csv");
+
+
 		bool loaded_dirty = load_image_from_file(dirty, 8, dirty_image);
 		bool loaded_psf = load_image_from_file(psf, 8, psf_image);
-		
+
 		// start timer
 		auto start = high_resolution_clock::now();
-		
-		int number_of_cycle=perform_clean(q, dirty, psf, local_max_x,
-			local_max_y, local_max_z, model_l, model_m, model_intensity, d_source_c,max_xyz,running_avg,operation_count);
-		
+
+		int number_of_cycle = perform_clean(q, dirty, psf, gain, iters, local_max_x,
+			local_max_y, local_max_z, model_l, model_m, model_intensity, d_source_c, max_xyz, running_avg, operation_count);
+
 		// stop timer
 		auto stop = high_resolution_clock::now();
 		auto duration = duration_cast<microseconds>(stop - start);
- 
+
 		// To get the value of duration use the count()
 		// member function on the duration object
-		cout << "The time taken is:" <<std::endl;
+		cout << "The time taken is:" << std::endl;
 		cout << duration.count() << std::endl;
-		cout << "The number of cycle is: ";
+
 		std::cout << number_of_cycle << "\n";
-		save_image_to_file(dirty,8, output_img);
-		save_sources_to_file(model_l,model_m,model_intensity,number_of_cycle,output_src);
+		save_image_to_file(dirty, 8, output_img);
+		save_sources_to_file(model_l, model_m, model_intensity, number_of_cycle, output_src);
 	}
 	catch (std::exception const& e) {
-		std::cout << "An exception is caught for cleaning.\n";
+		std::cout << "An exception is caught for clean.\n";
 		std::terminate();
 	}
 	return 0;
